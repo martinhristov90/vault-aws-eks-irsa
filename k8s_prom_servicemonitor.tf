77eb83b18f939484b551d474ebf05b5e3e496bcb
Martin Hristov <mhristov@hashicorp.com>
2024-12-26 12:36:53 +0200

    adding telemetry and changing Vault to ent

---
+++ k8s_prom_servicemonitor.tf
@@ -0,0 +1,48 @@
+resource "kubernetes_manifest" "servicemonitor_vault" {
+  manifest = {
+    "apiVersion" = "monitoring.coreos.com/v1"
+    "kind" = "ServiceMonitor"
+    "metadata" = {
+      "namespace" = "default"
+      "labels" = {
+        "app.kubernetes.io/instance" = "vault-prom-crd"
+        "app.kubernetes.io/managed-by" = "Helm"
+        "app.kubernetes.io/name" = "vault"
+        "helm.sh/chart" = "vault-0.28.1"
+        "release" = "marti-prom"
+      }
+      "name" = "vault-vault-server-${random_pet.env.id}"
+    }
+    "spec" = {
+      "endpoints" = [
+        {
+          "interval" = "30s"
+          "params" = {
+            "format" = [
+              "prometheus",
+            ]
+          }
+          "path" = "/v1/sys/metrics"
+          "port" = "http"
+          "scheme" = "http"
+          "scrapeTimeout" = "10s"
+          "tlsConfig" = {
+            "insecureSkipVerify" = true
+          }
+        },
+      ]
+      "namespaceSelector" = {
+        "matchNames" = [
+          "vault",
+        ]
+      }
+      "selector" = {
+        "matchLabels" = {
+          "app.kubernetes.io/instance" = "vault-server-${random_pet.env.id}"
+          "app.kubernetes.io/name" = "vault"
+          "vault-active" = "true"
+        }
+      }
+    }
+  }
+}
